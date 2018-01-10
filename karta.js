68af3a3b4c910109586d0ec6455f6f516e1db2d9
hariseminovic <con.hariseminovic@gmail.com>
2018-01-10 20:23:30 +0100

    Done n Done 20:23
    Nu ska det fanimej vara klart!

--- karta.js
+++ karta.js
@@ -1,13 +1,32 @@
-
-
 function initMap() {
-    var location = {lat: 55.704785, lng: 13.193841};
+    var lundagård = {lat: 55.704785, lng: 13.193841};
+    var bangatan = {lat: 55.705866, lng: 13.187604};
+
     var map = new google.maps.Map(document.getElementById("map"),{
         zoom: 16, 
-        center: location
+        center: lundagård
     });
-    var marker = new google.maps.Marker({
-        position: location, 
+    
+
+    var directionsDisplay = new google.maps.DirectionsRenderer({
         map: map
-    }); 
+      });
+
+    // Set destination, origin and travel mode.
+    var request = {
+        destination: bangatan,
+        origin: lundagård,
+        travelMode: 'DRIVING'
+      };
+
+      // Pass the directions request to the directions service.
+      var directionsService = new google.maps.DirectionsService();
+      directionsService.route(request, function(response, status) {
+        if (status == 'OK') {
+          // Display the route on the map.
+          directionsDisplay.setDirections(response);
+        }
+      });
+
 }
+
